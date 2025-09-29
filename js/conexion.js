698ff5c2f03b9912983a4a0ce5121c5e7a20d078
Nicolas-Q14 <nicolas.quitianr@uniagustiniana.edu.co>
2025-09-29 18:37:42 +0000

    arr button fav

--- js/conexion.js
+++ js/conexion.js
@@ -1,3 +1,23 @@
-function conexion () {
-    document.getElementById("root").innerHTML="conexion"
+function conexion() {
+    document.getElementById("root").innerHTML = "conexion";
 }
+
+let pokemones = [];
+let totalPokes = 1025;
+
+async function conexionLista() {
+const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${totalPokes}`);
+    const data = await res.json();
+    return data.results;
+}
+
+async function General() {
+    if (pokemones.length === 0) {
+        pokemones = await conexionLista();
+    }
+    
+    Home();
+    console.log(pokemones[2].name);
+}
+
+General();
--- js/home.js
+++ js/home.js
@@ -1,3 +1,12 @@
-function Home () {
-    document.getElementById("root").innerHTML= "Home"
+function home(parametro){
+    for (var i = 0; i < 9; i++) {
+        id = [i];
+        nombre = pokemones[i].name;
+        document.getElementById("root").innerHTML += `
+            <div class="un-pokemon" onclick="Detalle(${id})">
+                <p>${id} ${nombre}</p>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
+            </div>
+        `;
+    }
 }
