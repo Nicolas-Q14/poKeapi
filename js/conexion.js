9fff0fade2ca2d3aa80777b4be439f9a6aa96424
Nicolas-Q14 <nicolas.quitianr@uniagustiniana.edu.co>
2025-10-18 21:23:59 -0500

    Refactor conexion functions and update naming conventions

--- js/conexion.js
+++ js/conexion.js
@@ -1,46 +1,26 @@
-function conexion() {
-    document.getElementById("root").innerHTML = "conexion";
-}
-
 let pokemones = [];
-let totalPokes = 1025;
+let totalPokes = 1025; // corregido mayúscula
 
-async function conexionLista(unFiltro) {
-    if (unFiltro === "All") {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${totalPokes}`);
-        const data = await res.json();
-        return data.results; // [{name, url}, ...]
-    } else {
-        const res = await fetch(`https://pokeapi.co/api/v2/type/${unFiltro}`);
-        const data = await res.json();
-
-        // Estandarizamos el formato para que sea igual al de "All"
-        const pokemonesTipo = [];
-        for (let i = 0; i < data.pokemon.length; i++) {
-            pokemonesTipo.push({
-                name: data.pokemon[i].pokemon.name,
-                url: data.pokemon[i].pokemon.url
-            });
-        }
-        return pokemonesTipo;
-    }
+async function conexionlista(filtrotipo) {
+  if (filtrotipo === "all") {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${totalPokes}`);
+    const data = await res.json();
+    return data.results;
+  } else {
+    const res = await fetch(`https://pokeapi.co/api/v2/type/${filtrotipo}`);
+    const data = await res.json();
+    return data.pokemon.map(p => ({
+      name: p.pokemon.name,
+      url: p.pokemon.url
+    }));
+  }
 }
 
-async function General() {
-    if (pokemones.length === 0) {
-        pokemones = await conexionLista("All");
-    }
-    if (typeof home === "function") {
-        home(pokemones); 
-    }
-    console.log(pokemones[2].name);
+async function general() {
+  if (pokemones.length === 0) {
+    pokemones = await conexionlista("all");
+  }
+  home();
 }
 
-General();
-
-async function FiltroConexion(filtroelegido) {
-    let pokesFiltrados = await conexionLista(filtroelegido);
-    document.getElementById("la-lista").innerHTML = "";
-    let listaFiltro = GenerarLista(pokesFiltrados);
-    document.getElementById("la-lista").innerHTML = listaFiltro;
-}
+document.addEventListener("DOMContentLoaded", general);
